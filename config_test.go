2f4f9697dcf548ca385a03eda952e8a11e3d6d81
jspc <james+git@zero-internet.org.uk>
2022-10-01 16:24:45 +0100

    Cover lookup where a value is set

--- config_test.go
+++ config_test.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
 func TestLookup(t *testing.T) {
 	got := Lookup("__AN_UNSET_KEY_HOPEFULLY", "<3")
+	if got != "<3" {
+		t.Errorf("unexpected value %q", got)
+	}
+}
+
+func TestLookup_Exists(t *testing.T) {
+	os.Setenv("__THIS_KEY_HAS_A_VALUE", "<3")
+
+	got := Lookup("__THIS_KEY_HAS_A_VALUE", "blahblahblah")
 	if got != "<3" {
 		t.Errorf("unexpected value %q", got)
 	}
